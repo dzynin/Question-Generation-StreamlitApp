bcfd127ebb00473830a3451ec8fd3ef2a74254ae
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-05-03 17:48:26 +0530

    Update word_sim.py

--- word_sim.py
+++ word_sim.py
@@ -1,18 +1,8 @@
+############ Importing required libraries #################
+
 import altair as alt
 from PIL import Image
 import streamlit as st
-
-######################
-# Page Title
-######################
-
-# image = Image.open('C:\\Users\\Darcey\\Downloads\\DeepSphere Logo.jpg')
-
-# st.image(image)
-
-# st.write("""
-# Finding Similar Words using NLP
-# """)
 from gensim.models import Word2Vec
 # from nltk import sent_tokenize
 #from gensim.models.word2vec import LineSentence
@@ -25,9 +15,7 @@ import warnings
 warnings.filterwarnings('ignore')
 import re
 
-#def input_text():
-#    text1 = input('Input your text here and Press ENTER:')
-#    return text1
+########### Creating function to remove stopwords from the text ###############
 
 def remove_stopwords(text1):
     words = [word for word in text1.split() if word.lower() not in ENGLISH_STOP_WORDS]
@@ -37,11 +25,15 @@ def remove_stopwords(text1):
 #    text2 = LineSentence(text3)
     return text3
 
+########### Creating function to tokenize the text into sentences ###############
+
 def sent_tokenize(text3):
 #    if st.button('Click to Tokenize'):
 #    text2 = sent_tokenize(text3)
     text2 = re.compile('[.!?] ').split(text3)
     return text2
+
+########### Creating function to remove special characters and convert the text into lower case ###############
 
 def remove_special_Charac(text2):
     clean_text = []
@@ -49,11 +41,15 @@ def remove_special_Charac(text2):
         clean_text.append(gensim.utils.simple_preprocess(text))
     return clean_text
 
+########### Creating function to train the model using Word2Vec for the processed text ###############
+
 def Train_Model(clean_text):
 #    if st.button('Click to Train the model'):
     model = gensim.models.Word2Vec(clean_text, vector_size=300, window=5, min_count = 5, workers=10)
     model.train(clean_text, total_examples = len(clean_text), epochs=10)
     return model
+
+########### Creating function to apply the trained model ###############
 
 def Model_Outcome(model, word):
 #    if st.button('Click to Train the model'):
