02bcfabc156edf00f97cc7ca33a099574ed73ae5
DeepsphereAI <61693631+DeepsphereAI@users.noreply.github.com>
2021-05-04 10:03:21 +0530

    Bug fix

--- mcq.py
+++ mcq.py
@@ -85,11 +85,12 @@ def sense2vec_get_words(word):
     word = word.replace(" ", "_")
 
     sense = s2v.get_best_sense(word)
-    most_similar = s2v.most_similar(sense, n=20)
-    for each_word in most_similar:
-        append_word = each_word[0].split("|")[0].replace("_", " ").lower()
-        if append_word.lower() != word.lower():
-            output.append(append_word.title())
+    if sense:
+        most_similar = s2v.most_similar(sense, n=20)
+        for each_word in most_similar:
+            append_word = each_word[0].split("|")[0].replace("_", " ").lower()
+            if append_word.lower() != word.lower():
+                output.append(append_word.title())
 
     out = list(OrderedDict.fromkeys(output))
     return out
